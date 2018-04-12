d4d1994a089a873138e82b81a2abcdd33f45d46d
yujie-liu <yliu134@u.rochester.edu>
2018-04-12 11:09:01 -0400

    Small updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FCN-pytorch
+
+Pretrained model (tentative): included vgg16 and resnet101
+
+To-do: decide which resnet to use 
--- fcn/models/resNet.py
+++ fcn/models/resNet.py
@@ -1,1 +1,36 @@
 # The resNet model for classification
+#vgg16 model
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+from torchvision.models import resnet101
+
+class ResNet(resnet101):
+    def __init__(self, pretrained=True, model='resnet101', requires_grad=True, remove_fc=True):
+        super().__init__(make_layers(cfg[model]))
+        self.ranges = ranges[model]
+
+        if pretrained:
+            exec("self.load_state_dict(models.%s(pretrained=True).state_dict())" % model)
+
+        if not requires_grad:
+            for param in super().parameters():
+                param.requires_grad = False
+
+        if remove_fc:
+            del self.classifier
+
+    def forward(self, x):
+        output = {}
+
+        # get the output of each maxpooling layer
+        for idx in range(len(self.ranges)):
+            for layer in range(self.ranges[idx][0], self.ranges[idx][1]):
+                x = self.features[layer](x)
+            output["x%d"%(idx+1)] = x
+
+        return output
+
