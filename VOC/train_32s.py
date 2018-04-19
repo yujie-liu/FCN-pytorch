6c4df49c996e51b6a59e1e0d18735484700c4fad
yujie-liu <yliu134@u.rochester.edu>
2018-04-19 15:28:16 -0400

    Updated trainer for fcn32s

--- VOC/train_32s.py
+++ VOC/train_32s.py
@@ -1,162 +1,99 @@
-import argparse
-import datetime
-import os
-import os.path as osp
-import shlex
-import subprocess
 
-import pytz
+from __future__ import division
 import torch
-import yaml
+from torch.autograd import Variable
+from torch.utils import data
+from loss import CrossEntropy2d, CrossEntropyLoss2d
+from visualize import LinePlotter
+from transform import ReLabel, ToLabel, ToSP, Scale
+from torchvision.transforms import Compose, CenterCrop, Normalize, ToTensor
+import tqdm
+from PIL import Image
+import numpy as np
+import fcn
+from voc_loader2 import VOCSegmentation
 
-import torchfcn
+input_transform = Compose([
+    Scale((256, 256), Image.BILINEAR),
+    ToTensor(),
+    Normalize([.485, .456, .406], [.229, .224, .225]),
 
+])
+target_transform = Compose([
+    Scale((256, 256), Image.NEAREST),
+    ToSP(256),
+    ToLabel(),
+    ReLabel(255, 21),
+])
 
-configurations = {
-    # same configuration as original work
-    # https://github.com/shelhamer/fcn.berkeleyvision.org
-    1: dict(
-        max_iteration=1000,
-        lr=1.0e-10,
-        momentum=0.99,
-        weight_decay=0.0005,
-        interval_validate=4000,
-    )
-}
-
-
-# def git_hash():
-#     cmd = 'git log -n 1 --pretty="%h"'
-#     hash = subprocess.check_output(shlex.split(cmd)).strip()
-#     return hash
-#
-#
-# def get_log_dir(model_name, config_id, cfg):
-#     # load config
-#     name = 'MODEL-%s_CFG-%03d' % (model_name, config_id)
-#     for k, v in cfg.items():
-#         v = str(v)
-#         if '/' in v:
-#             continue
-#         name += '_%s-%s' % (k.upper(), v)
-#     now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
-#     name += '_VCS-%s' % git_hash()
-#     name += '_TIME-%s' % now.strftime('%Y%m%d-%H%M%S')
-#     # create out
-#     log_dir = osp.join(here, 'logs', name)
-#     if not osp.exists(log_dir):
-#         os.makedirs(log_dir)
-#     with open(osp.join(log_dir, 'config.yaml'), 'w') as f:
-#         yaml.safe_dump(cfg, f, default_flow_style=False)
-#     return log_dir
+trainloader = data.DataLoader(VOCSegmentation("../pascal-voc", img_transform=input_transform,
+                                         label_transform=target_transform),
+                              batch_size=16, shuffle=True, pin_memory=True)
 
 
-def get_parameters(model, bias=False):
-    import torch.nn as nn
-    modules_skipped = (
-        nn.ReLU,
-        nn.MaxPool2d,
-        nn.Dropout2d,
-        nn.Sequential,
-        torchfcn.models.FCN32s,
-        torchfcn.models.FCN16s,
-        torchfcn.models.FCN8s,
-    )
-    for m in model.modules():
-        if isinstance(m, nn.Conv2d):
-            if bias:
-                yield m.bias
-            else:
-                yield m.weight
-        elif isinstance(m, nn.ConvTranspose2d):
-            # weight is frozen because it is just a bilinear upsampling
-            if bias:
-                assert m.bias is None
-        elif isinstance(m, modules_skipped):
-            continue
+if torch.cuda.is_available():
+    model = fcn.models.fcn32s()
+    model.cuda()
+
+epoches = 5
+lr = 1e-4
+weight_decay = 2e-5
+momentum = 0.9
+weight = torch.ones(22)
+weight[21] = 0
+max_iters = 2*epoches
+
+criterion = CrossEntropyLoss2d(weight.cuda())
+optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum,
+                            weight_decay=weight_decay)
+ploter = LinePlotter()
+
+model.train()
+for epoch in range(epoches):
+    running_loss = 0.0
+    for i, (images, labels_group) in tqdm.tqdm(enumerate(trainloader)):
+        if torch.cuda.is_available():
+            images = [Variable(image.cuda()) for image in images]
+            labels_group = [labels for labels in labels_group]
         else:
-            raise ValueError('Unexpected module: %s' % str(m))
+            images = [Variable(image) for image in images]
+            labels_group = [labels for labels in labels_group]
+
+        optimizer.zero_grad()
+        losses = []
+        for img, labels in zip(images, labels_group):
+            outputs = model(img)
+            labels = [Variable(label.cuda()) for label in labels]
+            for pair in zip(outputs, labels):
+                losses.append(criterion(pair[0], pair[1]))
+
+        if epoch < 40:
+            loss_weight = [0.1, 0.1, 0.1, 0.1, 0.1, 0.5]
+        else:
+            loss_weight = [0.5, 0.1, 0.1, 0.1, 0.1, 0.1]
+
+        loss = 0
+        for w, l in zip(loss_weight, losses):
+            loss += w*l
+
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.data[0]
+
+        # lr = lr * (1-(92*epoch+i)/max_iters)**0.9
+        # for parameters in optimizer.param_groups:
+        #     parameters['lr'] = lr
+
+    print("Epoch [%d] Loss: %.4f" % (epoch+1, running_loss/i))
+    ploter.plot("loss", "train", epoch+1, running_loss/i)
+    running_loss = 0
+
+    if (epoch+1) % 2 == 0: # Orignially: every 20 epoch
+        lr /= 10
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr,
+                                    momentum=momentum,
+                                    weight_decay=weight_decay)
+        torch.save(model.state_dict(), "./pth/fcn-deconv-%d.pth" % (epoch+1))
 
 
-here = osp.dirname(osp.abspath(__file__))
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-g', '--gpu', type=int, required=True)
-    parser.add_argument('-c', '--config', type=int, default=1,
-                        choices=configurations.keys())
-    parser.add_argument('--resume', help='Checkpoint path')
-    args = parser.parse_args()
-
-    gpu = args.gpu
-    cfg = configurations[args.config]
-    out = get_log_dir('fcn32s', args.config, cfg)
-    resume = args.resume
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
-    cuda = torch.cuda.is_available()
-
-    torch.manual_seed(1337)
-    if cuda:
-        torch.cuda.manual_seed(1337)
-
-    # 1. dataset
-
-    root = osp.expanduser('~/data/datasets')
-    kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        torchfcn.datasets.SBDClassSeg(root, split='train', transform=True),
-        batch_size=1, shuffle=True, **kwargs)
-    val_loader = torch.utils.data.DataLoader(
-        torchfcn.datasets.VOC2011ClassSeg(
-            root, split='seg11valid', transform=True),
-        batch_size=1, shuffle=False, **kwargs)
-
-    # 2. model
-
-    model = torchfcn.models.FCN32s(n_class=21)
-    start_epoch = 0
-    start_iteration = 0
-    if resume:
-        checkpoint = torch.load(resume)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        start_epoch = checkpoint['epoch']
-        start_iteration = checkpoint['iteration']
-    else:
-        vgg16 = torchfcn.models.VGG16(pretrained=True)
-        model.copy_params_from_vgg16(vgg16)
-    if cuda:
-        model = model.cuda()
-
-    # 3. optimizer
-
-    optim = torch.optim.SGD(
-        [
-            {'params': get_parameters(model, bias=False)},
-            {'params': get_parameters(model, bias=True),
-             'lr': cfg['lr'] * 2, 'weight_decay': 0},
-        ],
-        lr=cfg['lr'],
-        momentum=cfg['momentum'],
-        weight_decay=cfg['weight_decay'])
-    if resume:
-        optim.load_state_dict(checkpoint['optim_state_dict'])
-
-    trainer = torchfcn.Trainer(
-        cuda=cuda,
-        model=model,
-        optimizer=optim,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        out=out,
-        max_iter=cfg['max_iteration'],
-        interval_validate=cfg.get('interval_validate', len(train_loader)),
-    )
-    trainer.epoch = start_epoch
-    trainer.iteration = start_iteration
-    trainer.train()
-
-
-if __name__ == '__main__':
-    main()
+torch.save(model.state_dict(), "./pth/fcn-deconv.pth")
