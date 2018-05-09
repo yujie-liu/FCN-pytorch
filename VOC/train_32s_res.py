71ff35a3af75b2ad9de8330c6baef67d9b9d2392
yujie-liu <yliu134@u.rochester.edu>
2018-05-09 10:33:33 -0400

    Formatting issue

--- VOC/train_32s_res.py
+++ VOC/train_32s_res.py
@@ -79,16 +79,16 @@ def get_parameters(model, bias=False):
         ResNet
     )
     for m in model.modules():
-	    if isinstance(m, nn.Conv2d):
+        if isinstance(m, nn.Conv2d):
             if bias:
                 yield m.bias
             else:
                 yield m.weight
         elif isinstance(m, nn.ConvTranspose2d):
-            # weight is frozen because it is just a bilinear upsampling
+        # weight is frozen because it is just a bilinear upsampling
             if bias:
                 assert m.bias is None
-	elif isinstance(m, modules_skipped):
+        elif isinstance(m, modules_skipped):
             continue
         else:
             raise ValueError('Unexpected module: %s' % str(m))
